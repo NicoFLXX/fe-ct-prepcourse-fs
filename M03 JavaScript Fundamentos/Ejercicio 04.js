274578ed0014807c96add4dc205f16c6774e8f50
nicoflx <florezznicolasxavier@gmail.com>
2023-10-10 10:25:24 -0300

    Tarea De Soy Henry

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -6,31 +6,43 @@
 function elevarAlCuadrado(num) {
    // Retorna el valor de "num" elevado al cuadrado.
    // Tu código:
+   var resultado = num * num;
+   return resultado;
 }
 
 function elevarAlCubo(num) {
    // Retorna el valor de "num" elevado al cubo.
    // Tu código:
+   var resultado = num * num * num;
+   return resultado;
 }
 
 function elevar(num, exponent) {
    // Retorna el valor de "num" elevado al exponente "exponent".
    // Tu código:
+   var resultado = Math.pow(num, exponent);
+   return resultado;
 }
 
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
+   var NumeroRedondeado = Math.round(num);
+   return NumeroRedondeado;
 }
 
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
    // Tu código:
+   var NumeroRedondeado = Math.ceil(num);
+   return NumeroRedondeado;
 }
 
 function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
+   var NumeroLoco = Math.random();
+   return NumeroLoco;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
